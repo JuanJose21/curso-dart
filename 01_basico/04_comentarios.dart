e0839db90eaa103ec4fc4402117b38cf9d54a3ea
JuanJose21 <jjsanchez689@gmail.com>
2024-04-12 16:37:10 -0500

    feat(comments): add exercise comments

---
+++ 01_basico/04_comentarios.dart
@@ -0,0 +1,30 @@
+/**
+ * Este archivo contiene ejemplos de comentarios en Dart
+ * [creadoEn] :  12/04/2024
+ */
+
+main() {
+  // Comentario de una línea
+  int a = 10; // Asignación de un valor a la variable a
+
+  /*
+    Comentario
+    de
+    varias
+    líneas
+  */
+  final personas = [
+    // Lista de personas
+    'Tony', // Tony Stark
+    'Peter', // Peter Parker
+    'Strange' // Doctor Strange
+  ];
+}
+
+// nombre: Nombre de la persona a saludar (De esta forma no se deberia de hacerlo)
+// mensaje: Mensaje de saludo
+
+/// Función para saludar a una persona (De esta forma se deberia de hacer)
+/// [nombre] Nombre de la persona a saludar
+/// [mensaje] Mensaje de saludo
+saludar(int nombre, int mensaje) {}
